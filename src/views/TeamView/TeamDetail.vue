bbde102825092d8f84832faea7feaab8f329cf2b
shiningxyone <2249027756@qq.com>
2024-08-03 17:57:56 +0800

    Update TeamDetail.vue

--- src/views/TeamView/TeamDetail.vue
+++ src/views/TeamView/TeamDetail.vue
@@ -4,15 +4,16 @@
     <p v-if="loading">Loading team data...</p>
     <div v-else>
       <div v-if="team">
-        <el-row>
-          <el-col :span="24">
+        <el-row>        
             <el-card shadow="never" class="box-card">
               <div slot="header" class="clearfix">
                 <span style="font-size: 20px;font-weight: bold;">球队简介</span>
                 <el-button type="primary" icon="el-icon-arrow-left" @click="goBack" class="back-button1">返回</el-button>
               </div>
               <el-row :gutter="20">
+              
                 <el-col :span="6">
+ 
                   <div class="team-image">
                     <img src="https://img.51miz.com/Element/00/91/85/34/422e1064_E918534_f98a4621.png" alt="Team Image" />
                   </div>
@@ -48,8 +49,8 @@
                 </el-col>
               </el-row>
             </el-card>
-          </el-col>
         </el-row>
+        <el-col :span="24" style="height: 20px;"></el-col> 
         <el-row >
           <el-col :span="24">
             <el-card shadow="never" class="box-card">
@@ -86,7 +87,8 @@
             </el-card>
           </el-col>
         </el-row>
-        <el-row>
+        <el-col :span="24" style="height: 20px;"></el-col> 
+        <el-row :gutter="20">
           <el-col :span="12">
             <el-card shadow="never" class="box-card">
               <div slot="header" class="clearfix">
@@ -142,7 +144,8 @@
             </el-card>
           </el-col>
         </el-row>
-        <el-row >
+        <el-col :span="24" style="height: 20px;"></el-col> 
+        <el-row :gutter="20">
           <el-col :span="12">
             <el-card shadow="never" class="box-card">
               <div slot="header" class="clearfix">
@@ -198,165 +201,44 @@
             </el-card>
           </el-col>
         </el-row>  
-
-
-
-        
-          <el-tab-pane label="阵容信息">
-  <el-container>
-    <el-main>
-      <el-input v-model="search" placeholder="输入搜索" clearable>
-        <i slot="prefix" class="el-input__icon el-icon-search"></i>
-      </el-input>
-      <el-table :data="lineup.filter(data => !search || data.NOTE.includes(search))" style="width: 100%">
-        <el-table-column prop="LINEUP_ID" label="阵容ID" width="150"></el-table-column>
-         <el-table-column prop="PLAYER1_ID" label="球员1ID" width="150">
-          <template #header>
-            <span>球员1ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER1_ID)">{{ scope.row.PLAYER1_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <!-- 其他球员ID列的代码类似，这里省略 -->
-        <el-table-column prop="PLAYER2_ID" label="球员2ID" width="150">
-          <template #header>
-            <span>球员2ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER2_ID)">{{ scope.row.PLAYER2_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER3_ID" label="球员3ID" width="150">
-          <template #header>
-            <span>球员3ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER3_ID)">{{ scope.row.PLAYER3_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER4_ID" label="球员4ID" width="150">
-          <template #header>
-            <span>球员4ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER4_ID)">{{ scope.row.PLAYER4_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER5_ID" label="球员5ID" width="150">
-          <template #header>
-            <span>球员5ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER5_ID)">{{ scope.row.PLAYER5_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER6_ID" label="球员6ID" width="150">
-          <template #header>
-            <span>球员6ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER6_ID)">{{ scope.row.PLAYER6_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER7_ID" label="球员7ID" width="150">
-          <template #header>
-            <span>球员7ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER7_ID)">{{ scope.row.PLAYER7_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER8_ID" label="球员8ID" width="150">
-          <template #header>
-            <span>球员8ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER8_ID)">{{ scope.row.PLAYER8_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER9_ID" label="球员9ID" width="150">
-          <template #header>
-            <span>球员9ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER9_ID)">{{ scope.row.PLAYER9_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER10_ID" label="球员10ID" width="150">
-          <template #header>
-            <span>球员10ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER10_ID)">{{ scope.row.PLAYER10_ID }}</el-button>
-          </template>
-        </el-table-column>
-        <el-table-column prop="PLAYER11_ID" label="球员11ID" width="150">
-          <template #header>
-            <span>球员11ID</span>
-          </template>
-          <template #default="scope">
-           <el-button type="text" @click="drawer = true,fetchplayerdata(scope.row.PLAYER11_ID)">{{ scope.row.PLAYER11_ID }}</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-main>
-  </el-container>
-          </el-tab-pane>
-
-
-           <el-tab-pane label="比赛信息">
-            <el-container>
-              <el-main>
-                <el-input v-model="search" placeholder="输入搜索" clearable>
-                <i slot="prefix" class="el-input__icon el-icon-search"></i>
-                </el-input>
-                <el-table :data="match.filter(data => !search || data.NOTE.includes(search))" style="width: 100%">
-                  <el-table-column prop="MATCH_ID" label="比赛ID" width="150">
-                  </el-table-column>
-                  <el-table-column prop="MATCH_DATE" label="比赛日期" width="150">
-                  </el-table-column>
-                  <el-table-column prop="MATCH_STADIUM" label="比赛地点" width="150">
-                  </el-table-column>
-                  <el-table-column prop="HOME_TEAM_NAME" label="主场球队名称" width="150">
-                  </el-table-column>
-                  <el-table-column prop="AWAY_TEAM_NAME" label="客场球队名称" width="150" >
-                  </el-table-column>
-                  <el-table-column prop="HOME_TEAM_SCORE" label="主场得分" width="150" >
-                  </el-table-column>
-                  <el-table-column prop="AWAY_TEAM_SCORE" label="客场得分" width="150" >
-                  </el-table-column>
-                  <el-table-column prop="HOME_TEAM_ID" label="主场球队ID"  width="150">
-                  </el-table-column>
-                  <el-table-column prop="AWAY_TEAM_ID" label="客场球队ID" width="150" >
-                  </el-table-column>
-                </el-table>
-              </el-main>    
-            </el-container>
-          </el-tab-pane>
-           <el-tab-pane label="医疗信息"  >
-            <el-container>
-              <el-main>
-                <el-input v-model="search" placeholder="输入搜索" clearable>
-                <i slot="prefix" class="el-input__icon el-icon-search"></i>
-                </el-input>
-                <el-table :data="medical.filter(data => !search || data.NOTE.includes(search))" style="width: 100%">
-                  <el-table-column prop="medical_id" label="医疗处理ID" width="150" >
-                  </el-table-column>
-                  <el-table-column prop="player_id" label="球员ID" width="150">
-                  </el-table-column>
-                  <el-table-column prop="hurt_part" label="损伤部位" width="150">
-                  </el-table-column>
-                  <el-table-column prop="hurt_time" label="受伤时间" width="150">
-                  </el-table-column>
-                  <el-table-column prop="medical_care" label="处理方法" width="150" >
-                  </el-table-column>
-                  <el-table-column prop="state" label="处理结果" width="150" >
-                  </el-table-column>
-                </el-table>
-              </el-main>    
-            </el-container>
-          </el-tab-pane>
+        <el-col :span="24" style="height: 20px;"></el-col> 
+        <el-row gutter="20">
+          <el-col :span="6" >
+            <el-card :body-style="{ padding: '14px' }" shadow="hover" @click="handleClickRecord">
+              <img src="" class="image">
+                <div style="padding: 14px;">
+                  <span >财务记录</span>
+                </div>
+            </el-card>
+          </el-col>
+          <el-col :span="6" >
+            <el-card :body-style="{ padding: '14px' }" shadow="hover" @click="handleClickMatch">
+              <img src="" class="image">
+                <div style="padding: 14px;">
+                  <span>赛事信息</span>
+                    
+                </div>
+            </el-card>
+          </el-col>
+          <el-col :span="6" >
+            <el-card :body-style="{ padding: '14px' }" shadow="hover" @click="handleClickLineup">
+              <img src="" class="image">
+                <div style="padding: 14px;">
+                  <span>阵容信息</span>
+                    
+                </div>
+            </el-card>
+          </el-col>
+          <el-col :span="6" >
+            <el-card :body-style="{ padding: '14px' }" shadow="hover" @click="handleClickMedical">
+              <img src="" class="image">
+                <div style="padding: 14px;">
+                  <span>医疗记录</span>
+                   
+                </div>
+            </el-card>
+          </el-col>
+        </el-row>
       </div>
       <div v-else>
         <p>No team data available.</p>
@@ -439,13 +321,13 @@
       },
   },
     methods: {
-      handlePlayerDetails(row) {
+    handlePlayerDetails(row) {
       console.log('Navigating to player detail page for:', row.PLAYER_ID);
       const playerId = row.PLAYER_ID;
       const route = `/player-display/${playerId}`;
       this.$router.push(route);
     },
-      handleSizeChange1(val) {
+    handleSizeChange1(val) {
       this.pageSize1 = val;
       this.currentPage1 = 1; // 每次改变每页条数时，重置当前页为第一页
     },
@@ -459,7 +341,7 @@
     handleCurrentChange2(val) {
       this.currentPage2 = val;
     },
-      fetchTeamDetail() {
+    fetchTeamDetail() {
         const teamID = this.$route.params.teamID;
         console.log('Fetching data for team ID:', teamID);
         axios.get(`/api/v1/team/displayone?Teamid=${teamID}`)
@@ -523,7 +405,7 @@
           this.loading = false;
         })
       },
-      goBack() {
+    goBack() {
       this.$router.go(-1); // Navigate to the previous page
     }
     },
@@ -560,6 +442,7 @@
 .el-tag {
   margin-left: 1rem;
 }
+
 .box-card {
     width: 100%;
     height: 370px;
@@ -571,7 +454,33 @@
   }
   .back-button1 {
   float: right;
+}
+.bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
 
- 
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    height:200px;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  
+  .clearfix:after {
+      clear: both
+  }
+  .el-card .el-card__body {
+  text-align: center;
 }
   </style>
