3acf2a04f8cb95b08aa8819d1d154d7abe9bd025
Ericufo <2252584@tongji.edu.cn>
2024-08-20 20:19:17 +0800

    Update PlayerDisplay.vue

--- src/views/PlayerView/PlayerDisplay.vue
+++ src/views/PlayerView/PlayerDisplay.vue
@@ -26,7 +26,12 @@
             </el-row>
             <el-row>
               <el-col :span="24">
-                <el-tag type="info">{{ player[0].BIRTHDAY }}</el-tag>
+                <el-tag type="info"
+                  >{{ player[0].BIRTHDAY }} ({{
+                    calculateAge(player[0].BIRTHDAY)
+                  }}
+                  岁)</el-tag
+                >
               </el-col>
             </el-row>
           </el-card>
@@ -38,7 +43,18 @@
             <div slot="header" class="clearfix">
               <span>球队</span>
             </div>
-            <el-tag type="info">{{ player[0].TEAM_NAME }}</el-tag>
+            <el-row>
+              <el-col :span="24">
+                <el-tag type="info">{{ player[0].TEAM_NAME }}</el-tag>
+              </el-col>
+              <el-col>
+                <img
+                  src="https://img.51miz.com/Element/00/91/85/34/422e1064_E918534_f98a4621.png"
+                  alt="Team Image"
+                  class="team-logo"
+                />
+              </el-col>
+            </el-row>
           </el-card>
         </el-col>
 
@@ -95,39 +111,102 @@
       <!-- 第三层：医疗，阵容，比赛，转会 -->
       <el-row :gutter="20" class="bottom-layer">
         <el-col :span="6">
-          <el-card class="module-card medical-card" shadow="hover">
+          <el-card class="module-card3" shadow="hover">
             <p>Medical</p>
-            <!-- 横向排列医疗记录 -->
-            <div class="medical-records-container" v-if="tableData.length">
-              <div
-                v-for="record in tableData"
-                :key="record.MEDICAL_ID"
-                class="medical-record"
-              >
-                <p>ID: {{ record.MEDICAL_ID }}</p>
-                <p>Hurt: {{ record.HURT_PART }}</p>
-                <p>Time: {{ record.HURT_TIME }}</p>
-                <p>Care: {{ record.MEDICAL_CARE }}</p>
-                <p>State: {{ record.STATE }}</p>
-                <p>Recovery: {{ record.PRED_REC_DATE }}</p>
-              </div>
-            </div>
+            <el-table
+              :data="tableData"
+              style="width: 100%"
+              v-if="tableData.length"
+              height="120"
+            >
+              <el-table-column
+                prop="MEDICAL_ID"
+                label="ID"
+                width="80"
+              ></el-table-column>
+              <el-table-column
+                prop="HURT_PART"
+                label="受伤部位"
+              ></el-table-column>
+              <el-table-column
+                prop="MEDICAL_CARE"
+                label="医疗建议"
+              ></el-table-column>
+              <el-table-column label="受伤天数" width="120">
+                <template slot-scope="scope">
+                  <span
+                    >{{
+                      calculateDaysDifference(
+                        scope.row.HURT_TIME,
+                        scope.row.PRED_REC_DATE
+                      )
+                    }}
+                    days</span
+                  >
+                </template>
+              </el-table-column>
+            </el-table>
             <p v-else>No medical records found for this player.</p>
           </el-card>
         </el-col>
         <el-col :span="6">
-          <el-card class="module-card" shadow="hover">
+          <el-card class="module-card3" shadow="hover">
             <p>Line-up</p>
+            <el-table
+              :data="lineupData"
+              style="width: 100%"
+              v-if="lineupData.length"
+              height="120"
+            >
+              <el-table-column
+                prop="LINEUP_ID"
+                label="ID"
+                width="80"
+              ></el-table-column>
+              <el-table-column prop="TEAM_NAME" label="球队"></el-table-column>
+              <el-table-column
+                prop="NOTE"
+                label="备注"
+                width="100"
+              ></el-table-column>
+            </el-table>
+            <p v-else>No lineup records found for this player.</p>
           </el-card>
         </el-col>
         <el-col :span="6">
-          <el-card class="module-card" shadow="hover">
+          <el-card class="module-card3" shadow="hover">
             <p>Match</p>
           </el-card>
         </el-col>
         <el-col :span="6">
-          <el-card class="module-card" shadow="hover">
+          <el-card class="module-card3" shadow="hover">
             <p>Transfer</p>
+            <el-table
+              :data="transferData"
+              style="width: 100%"
+              v-if="transferData.length"
+              height="120"
+            >
+              <el-table-column
+                prop="transfer_id"
+                label="ID"
+                width="60"
+              ></el-table-column>
+              <el-table-column
+                prop="team_id_from"
+                label="From"
+              ></el-table-column>
+              <el-table-column prop="team_id_to" label="To"></el-table-column>
+              <el-table-column
+                prop="transfer_fees"
+                label="Fees"
+              ></el-table-column>
+              <el-table-column
+                prop="transfer_date"
+                label="Date"
+              ></el-table-column>
+            </el-table>
+            <p v-else>No transfer records found for this player.</p>
           </el-card>
         </el-col>
       </el-row>
@@ -141,6 +220,8 @@ import axios from "axios";
 export default {
   data() {
     return {
+      transferData: [],
+      lineupData: [],
       tableData: [],
       allData: [],
       positionStyle: {
@@ -158,10 +239,68 @@ export default {
     };
   },
   created() {
+    this.fetchTransferRecords();
+    this.fetchLineupRecords();
     this.fetchPlayerData();
     this.fetchMedicalRecords();
   },
   methods: {
+    calculateAge(birthday) {
+      const birthDate = new Date(birthday);
+      const today = new Date();
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+
+      if (
+        monthDiff < 0 ||
+        (monthDiff === 0 && today.getDate() < birthDate.getDate())
+      ) {
+        age--;
+      }
+
+      return age;
+    },
+
+    calculateDaysDifference(hurtTime, recoveryDate) {
+      const hurtDate = new Date(hurtTime).setHours(0, 0, 0, 0);
+      const recovery = new Date(recoveryDate).setHours(0, 0, 0, 0);
+      const difference = (recovery - hurtDate) / (1000 * 60 * 60 * 24); // 转换为天数
+      return difference;
+    },
+
+    fetchTransferRecords() {
+      const playerId = this.$route.params.playerId;
+      if (playerId) {
+        axios
+          .get(`/api/v1/transfer/displayall?playerid=${playerId}`)
+          .then((response) => {
+            console.log("Received transfer data:", response.data);
+            this.transferData = response.data.sort(
+              (a, b) => a.transfer_id - b.transfer_id
+            );
+          })
+          .catch((error) => {
+            console.error("Failed to fetch transfer list:", error);
+          });
+      }
+    },
+    fetchLineupRecords() {
+      const playerId = this.$route.params.playerId;
+      if (playerId) {
+        axios
+          .get(`/api/v1/lineup/displayall?playerid=${playerId}`)
+          .then((response) => {
+            console.log("Received lineup data:", response.data);
+            this.lineupData = response.data.sort(
+              (a, b) => a.LINEUP_ID - b.LINEUP_ID
+            );
+          })
+          .catch((error) => {
+            console.error("Failed to fetch lineup list:", error);
+          });
+      }
+    },
+
     fetchMedicalRecords() {
       const playerId = this.$route.params.playerId;
       if (playerId) {
@@ -239,6 +378,33 @@ export default {
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
 }
 
+.module-card2 {
+  width: 600px;
+  height: 300px;
+  padding: 10px;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  text-align: center;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+}
+
+.module-card3 {
+  width: 400px;
+  height: 300px;
+  padding: 10px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  text-align: center;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+}
+
 .medical-card {
   height: auto;
   overflow-y: auto;
@@ -261,19 +427,6 @@ export default {
 .medical-record p {
   margin: 2px 0;
   font-size: 10px; /* 更小的字体 */
-}
-
-.module-card2 {
-  width: 600px;
-  height: 300px;
-  padding: 10px;
-  margin: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 5px;
-  text-align: center;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
 }
 
 .el-divider {
@@ -306,5 +459,22 @@ export default {
 .info-row {
   display: flex;
   justify-content: space-between;
+}
+
+.team-image {
+  width: 100px; /* 容器宽度 */
+  height: 100px; /* 容器高度 */
+  background-color: #f2f2f2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.team-logo {
+  width: 50%; /* 图像宽度 */
+  height: 50%; /* 图像高度 */
+  object-fit: cover; /* 保持图像比例，剪裁多余部分 */
 }
 </style>
