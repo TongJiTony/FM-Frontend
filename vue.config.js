01a7ec7b0a0792ca3b92336fbea5752b66b41fa8
Zixun Huang <2153679@mail.tongji.edu.cn>
2024-09-05 12:22:31 +0800

    Use backend server as proxy target

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // 后端API的URL
+        target: 'http://www.tonyhuang0908.com:8080', // 后端API的URL
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
